7ea25cf6c5a7548e82935a4c91eb28e00bce825c
kumarshubu <shubhamkumaar@gmail.com>
2019-05-30 12:45:07 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Main from "./components/Main";
+import Secret from "./components/Secret";
+import NotFound from "./components/NotFound";
+import CallBack from "./components/CallBack";
 
-function App() {
+class App extends React.Component {
+  render(){
+    let mainComponent="";
+    switch(this.props.location){
+      case "":
+        mainComponent=<Main {...this.props}/>
+        break;
+      case "callback":
+        mainComponent=<CallBack/>
+        break;
+      case "secret":
+        mainComponent= this.props.auth.isAuthenticated()?<Secret {...this.props}/>:<NotFound/>;
+        break;
+      default:
+        mainComponent=<NotFound/>;
+    }
   return (
     <div className="App">
+      {mainComponent}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Welcome to React, {this.props.name}</h1>
       </header>
     </div>
   );
+}
 }
 
 export default App;
